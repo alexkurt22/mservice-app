22c0e8813ccb83be26447a1e975103f338449a07
alexkurt22 <alchpan1997@gmail.com>
2026-07-14 13:51:51 +0500

    Create home_screen.dart

---
+++ lib/home_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'login_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Главный экран'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              final prefs = await SharedPreferences.getInstance();
+              await prefs.clear();
+              if (context.mounted) {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (_) => const LoginScreen()),
+                );
+              }
+            },
+          )
+        ],
+      ),
+      body: const Center(
+        child: Text(
+          'Главный экран',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
