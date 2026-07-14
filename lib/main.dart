7e1892221440f65983d9f99072b0300396420909
alexkurt22 <alchpan1997@gmail.com>
2026-07-14 11:05:32 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -10,14 +16,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'M-Service Client',
+      title: 'M-Service',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const Scaffold(
         body: Center(
-          child: Text('M-Service Client: Каркас запущен!'),
+          child: Text('M-Service Client: Firebase успешно подключен!'),
         ),
       ),
     );
