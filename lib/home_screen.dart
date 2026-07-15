2a5f9f41eec184f8dacf7fe2bcae77359a8c7b8d
alexkurt22 <alchpan1997@gmail.com>
2026-07-15 22:12:16 +0500

    Update home_screen.dart

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -136,7 +136,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => const CreateOrderScreen(),
+                      builder: (context) => CreateOrderScreen(),
                     ),
                   );
                 },
