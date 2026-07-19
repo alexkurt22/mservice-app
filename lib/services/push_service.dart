0edd0f5cc23d93ef8f2aeefbd50988c9c956676c
alexkurt22 <alchpan1997@gmail.com>
2026-07-19 22:10:33 +0500

    Update push_service.dart

--- lib/services/push_service.dart
+++ lib/services/push_service.dart
@@ -1,31 +1,25 @@
 import 'dart:convert';
+import 'package:flutter/services.dart';
 import 'package:googleapis_auth/auth_io.dart';
 import 'package:http/http.dart' as http;
-import '../secret/firebase_secret.dart'; // ❗ ПОДКЛЮЧАЕМ НАШ СГЕНЕРИРОВАННЫЙ КЛЮЧ
 
 class PushService {
   static Future<String> sendPushToAdmins(String title, String body) async {
     try {
-      final accountCredentials = ServiceAccountCredentials.fromJson(firebaseSecret);
+      final jsonString = await rootBundle.loadString('assets/firebase_credentials.json');
+      final accountCredentials = ServiceAccountCredentials.fromJson(jsonString);
       final scopes = ['https://www.googleapis.com/auth/firebase.messaging'];
       final client = await clientViaServiceAccount(accountCredentials, scopes);
-      final projectId = jsonDecode(firebaseSecret)['project_id'];
+      final projectId = jsonDecode(jsonString)['project_id'];
 
       final url = 'https://fcm.googleapis.com/v1/projects/$projectId/messages:send';
       final payload = {
         'message': {
           'topic': 'admins',
-          'notification': {
-            'title': title,
-            'body': body,
-          },
+          'notification': { 'title': title, 'body': body },
           'android': {
             'priority': 'high',
-            'notification': {
-              'sound': 'default',
-              'default_vibrate_timings': true,
-              'default_sound': true
-            }
+            'notification': { 'sound': 'default', 'default_vibrate_timings': true, 'default_sound': true }
           }
         }
       };
@@ -35,15 +29,10 @@ class PushService {
         headers: {'Content-Type': 'application/json'},
         body: jsonEncode(payload),
       );
-
       client.close();
-      if (response.statusCode == 200) {
-        return 'Успех (200)';
-      } else {
-        return 'Ошибка Google: ${response.statusCode} ${response.body}';
-      }
+      return response.statusCode == 200 ? 'Успех (200)' : 'Ошибка: ${response.statusCode} ${response.body}';
     } catch (e) {
-      return 'Сбой кода: $e';
+      return 'Сбой: $e';
     }
   }
 }
